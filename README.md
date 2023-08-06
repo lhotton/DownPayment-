4a2f1aaa14d349b5f34c29fc7d1e0d9d3b63f4d6
lhotton <119619562+lhotton@users.noreply.github.com>
2023-08-06 15:18:52 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # DownPayment-
 Program that calculates percentage of monthly salary you need to save in order to afford down payment for house in given amount of time. 
+
+Inputs: 
+
+Total cost of home
+down payment as percentage of total home cost in decimal form
+annual rate of return on savings/investments in decimal form
+semi-annual raise in decimal form
+annual salary
+the amount of time in which you want to be able to afford the downpayment in months
+
+Outputs:
+
+percentage of monthly salary you should save in order to afford down payment in given amount of time
+
+OR
+
+if salary is too low to afford in given amount of time, the program will tell you
+
+
---
+++ downpayment.py
@@ -0,0 +1,37 @@
+total_cost = float(input('Enter total cost of home: '))
+portion_down_payment = float(input('Enter portion of total price for down payment: '))
+r = float(input('Enter expexted rate of return for investment: '))
+semi_annual_raise = float(input('Enter semi annual raise: '))
+annual_salary = float(input('Enter annual salary: '))
+time = int(input('In how many months do you want to pay down payment?: '))
+
+down_payment = portion_down_payment*total_cost
+
+#calculates current_savings based on portion_saved determined from bisection
+def calculate_current_savings(portion_saved):
+    monthly_salary = annual_salary/12
+    current_savings = 0
+    for month in range(time):
+        if month % 6 == 0 and month != 0: #raise every 6 months
+            monthly_salary += monthly_salary * semi_annual_raise
+        current_savings += current_savings*r/12 + monthly_salary*portion_saved
+    return current_savings
+
+if calculate_current_savings(1) >= down_payment: #checks to see if you can afford downpayment by saving 100% of salary
+    epsilon = 100
+    low = 0
+    high = 10000
+    portion_saved = (low + high)/2/10000
+    num_guesses = 0
+
+    while abs(calculate_current_savings(portion_saved)-down_payment) >= epsilon: #loop ends once you are 100$ within price of downpayement
+        if calculate_current_savings(portion_saved) > down_payment:
+            high = portion_saved * 10000
+        else:
+            low = portion_saved * 10000
+        portion_saved = (low + high)/2/10000
+        num_guesses += 1
+    
+    print('you should save',str(round((portion_saved*100),2))+'%','of your monthly salary')
+else:
+    print('It is not possible to pay the down payment in given amount of time')
